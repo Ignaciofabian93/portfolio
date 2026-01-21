f8aa426b27f787047cdc6eb68c3959a39a25a419
Ignaciofabian93 <ignaciofabian93@gmail.com>
2026-01-20 21:56:30 -0300

    og spacing name

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -130,7 +130,7 @@ export default async function Image() {
           gap: "16px",
           flexWrap: "wrap",
           maxWidth: "750px",
-          marginBottom: "80px",
+          marginBottom: "64px",
         }}
       >
         {["Next.js", "GraphQL", "TypeScript", "Microservices", "AWS"].map(
